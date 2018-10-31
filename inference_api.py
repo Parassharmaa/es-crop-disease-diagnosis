68ae08dc09b424b5d12f92b51e876841e171bbd6
paraz <mail2paras.s@gmail.com>
2018-10-31 23:10:03 +0530

    web app init

---
+++ inference_api.py
@@ -0,0 +1,31 @@
+import os
+from flask import Flask, render_template, request
+from neural_kb import predict
+from fact_base import get_facts
+import json
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = os.path.basename('uploads')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    file = request.files['image']
+    f = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+
+    file.save(f)
+
+    disease_id, label, prediction = predict(image_path)
+
+    return json.dumps(get_facts(disease_id))
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
