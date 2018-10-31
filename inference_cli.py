0a1f74b6810906a71b5ca4c762b6d965b9dc68ec
paraz <mail2paras.s@gmail.com>
2018-10-31 19:02:39 +0530

    kb, fact_base, inference complete

---
+++ inference_cli.py
@@ -0,0 +1,17 @@
+from neural_kb import predict
+from fact_base import get_facts
+import argparse
+
+
+ap = argparse.ArgumentParser()
+ap.add_argument('-i', '--image', required=True,
+                help='path to side input image')
+
+args = ap.parse_args()
+
+
+image_path = args.image
+
+disease_id, label, prediction = predict(image_path)
+
+print(get_facts(disease_id))
